3b9166e8cb27ae6f7c642d6997ba418f0dd1387a
arun0179 <kanyanat.i@ku.th>
2020-09-14 00:50:36 +0700

    first commit spring annotation:atm

---
+++ src/main/java/th/ac/ku/atm/Bank.java
@@ -0,0 +1,34 @@
+package th.ac.ku.atm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component //component เพื่อสร้าง bean
+public class Bank {
+    private String name;
+
+    //composition
+    private Map<Integer,Customer> customers;
+//    private DataSourceDB dataSourceDB;
+    private DataSource dataSourceFile;
+
+    //receive the dataSource like this it is like you can change object
+    @Autowired //auto doing dependency injection
+    public Bank(@Value("${bankname}") String name, DataSource dataSource) {
+        this.name = name;
+        this.customers = dataSource.readCustomers();
+    }
+    public void registerCustomer(Customer customer){
+        this.customers.put(customer.getId(),customer);
+    }
+    public Customer findCustomer(int id){
+        return customers.get(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
